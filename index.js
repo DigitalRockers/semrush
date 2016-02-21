8f50a6e5615bbfa783bbc1e0c2f50226a610d762
Gianluca Pengo <gianluca.pengo@gmail.com>
2016-02-21 17:04:07 +0100

    fix methods generation

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var request = require('request');
 var _ = require('lodash');
 
 var baseApiUrl = 'http://api.semrush.com/';
+var advApiUrl = 'http://api.asns.backend.semrush.com/';
+var backlinksApiUrl = 'http://api.semrush.com/analytics/v1/';
 
 
 var Semrush = function(options){
@@ -28,18 +30,19 @@ Semrush.prototype.__baseCall = function(apiUrl, type, options, defaults, callbac
 		defaults = {};
 	}
 
-	var self = this,	
+	var self = this,
 		query = {
 			type: type,
-			key: self.apiKey,		
+			key: self.apiKey,
 		};
+
 	query = _.merge(_.merge(defaults, options), query);
 
 	var t = Date.now();
 	request({url: apiUrl, qs: query}, function(error, response, json){
 			self.__debug('GET - ' + apiUrl + ' - ' + (Date.now() - t) + ' ms');
 
-			if(error) return callback(error);	
+			if(error) return callback(error);
 
 			self.__debugInspect(json);
 
@@ -51,58 +54,151 @@ Semrush.prototype.__baseCall = function(apiUrl, type, options, defaults, callbac
 
 var methods = {
 	//Overview Reports
-	domain_ranks: null,
-	domain_rank: null,
-	domain_rank_history: null ,
-	rank_difference: null,
-	rank: null,
+	domain_ranks: {
+		apiUrl: baseApiUrl,
+	},
+	domain_rank: {
+		apiUrl: baseApiUrl,
+	},
+	domain_rank_history: {
+		apiUrl: baseApiUrl,
+	} ,
+	rank_difference: {
+		apiUrl: baseApiUrl,
+	},
+	rank: {
+		apiUrl: baseApiUrl,
+	},
 
 	//Domain Reports
-	domain_organic: null,
-	domain_adwords: null,
-	domain_adwords_unique: null,
-	domain_organic_organic: null,
-	domain_adwords_adwords: null,
-	domain_adwords_historical: null,
-	domain_domains: null,
-	domain_shopping: null,
-	domain_shopping_unique: null,
-	domain_shopping_shopping: null,
+	domain_organic: {
+		apiUrl: baseApiUrl,
+	},
+	domain_adwords: {
+		apiUrl: baseApiUrl,
+	},
+	domain_adwords_unique: {
+		apiUrl: baseApiUrl,
+	},
+	domain_organic_organic: {
+		apiUrl: baseApiUrl,
+	},
+	domain_adwords_adwords: {
+		apiUrl: baseApiUrl,
+	},
+	domain_adwords_historical: {
+		apiUrl: baseApiUrl,
+	},
+	domain_domains: {
+		apiUrl: baseApiUrl,
+	},
+	domain_shopping: {
+		apiUrl: baseApiUrl,
+	},
+	domain_shopping_unique: {
+		apiUrl: baseApiUrl,
+	},
+	domain_shopping_shopping: {
+		apiUrl: baseApiUrl,
+	},
 
 	//Keywords Reports
-	phrase_all: null,
-	phrase_this: null,
-	phrase_organic: null,
-	pharse_adwords: null,
-	phrase_related: null,
-	pharse_adwords_historical: null,
-	phrase_fullsearch: null,
-	phrase_kdi: null,
+	phrase_all: {
+		apiUrl: baseApiUrl,
+	},
+	phrase_this: {
+		apiUrl: baseApiUrl,
+	},
+	phrase_organic: {
+		apiUrl: baseApiUrl,
+	},
+	pharse_adwords: {
+		apiUrl: baseApiUrl,
+	},
+	phrase_related: {
+		apiUrl: baseApiUrl,
+	},
+	pharse_adwords_historical: {
+		apiUrl: baseApiUrl,
+	},
+	phrase_fullsearch: {
+		apiUrl: baseApiUrl,
+	},
+	phrase_kdi: {
+		apiUrl: baseApiUrl,
+	},
 
 	//URL reports
-	url_organic: null,
-	url_adwords: null,
+	url_organic: {
+		apiUrl: baseApiUrl,
+	},
+	url_adwords: {
+		apiUrl: baseApiUrl,
+	},
 
 	//Display Advertising Reports
-	publisher_text_ads: {action: 'report', export: 'api'},
-	publisher_advertiser: {action: 'report', export: 'api'},
-	publihser_publihsers: {action: 'report', export: 'api'},
-	advertiser_publisher: {action: 'report', export: 'api'},
-	advertiser_text_ads: {action: 'report', export: 'api'},
-	advertiser_landings: {action: 'report', export: 'api'},
-	advertiser_publisher_text_ads: {action: 'report', export: 'api'},
-	advertiser_rank: {action: 'report', export: 'api'},
-	publihser_rank: {action: 'report', export: 'api'},
+	publisher_text_ads: {
+		apiUrl: advApiUrl,
+		params: {action: 'report', export: 'api'}
+	},
+	publisher_advertiser: {
+		apiUrl: advApiUrl,
+		params: {action: 'report', export: 'api'},
+	},
+	publihser_publihsers: {
+		apiUrl: advApiUrl,
+		params: {action: 'report', export: 'api'},
+	},
+	advertiser_publisher: {
+		apiUrl: advApiUrl,
+		params: {action: 'report', export: 'api'},
+	},
+	advertiser_text_ads: {
+		apiUrl: advApiUrl,
+		params: {action: 'report', export: 'api'},
+	},
+	advertiser_landings: {
+		apiUrl: advApiUrl,
+		params: {action: 'report', export: 'api'},
+	},
+	advertiser_publisher_text_ads: {
+		apiUrl: advApiUrl,
+		params: {action: 'report', export: 'api'},
+	},
+	advertiser_rank: {
+		apiUrl: advApiUrl,
+		params: {action: 'report', export: 'api'},
+	},
+	publihser_rank: {
+		apiUrl: advApiUrl,
+		params: {action: 'report', export: 'api'},
+	},
 
 	//Backlinks
-	backlinks_overview: null,
-	backlinks: null,
-	backlinks_refdomains: null,
-	backlinks_refips: null,
-	backlinks_tld: null,
-	backlinks_geo: null,
-	backlinks_anchors: null,
-	backlinks_pages: null
+	backlinks_overview: {
+		apiUrl: backlinksApiUrl,
+	},
+	backlinks: {
+		apiUrl: backlinksApiUrl,
+	},
+	backlinks_refdomains: {
+		apiUrl: backlinksApiUrl,
+	},
+	backlinks_refips: {
+		apiUrl: backlinksApiUrl,
+	},
+	backlinks_tld: {
+		apiUrl: backlinksApiUrl,
+	},
+	backlinks_geo: {
+		apiUrl: backlinksApiUrl,
+	},
+	backlinks_anchors: {
+		apiUrl: backlinksApiUrl,
+	},
+	backlinks_pages: {
+		apiUrl: backlinksApiUrl,
+	}
 };
 
 var methodName = function(name){
@@ -113,15 +209,11 @@ var methodName = function(name){
 };
 
 for(var i in methods){
-	if(methods[i]){
-		Semrush.prototype[methodName(i)] = function(options, callback){			
-			this.__baseCall(baseApiUrl, i, options, methods[i], callback);
+	(function(url, name, i, defaults) {
+		Semrush.prototype[name] = function(options, callback){
+			this.__baseCall(url, i, options, defaults, callback);
 		};
-	} else {
-		Semrush.prototype[methodName(i)] = function(options, callback){			
-			this.__baseCall(baseApiUrl, i, options, callback);
-		};
-	}
+	})(methods[i].apiUrl, methodName(i), i, methods[i].params || {});
 }
 
 
